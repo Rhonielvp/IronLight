4cc140a1119cd3833bda85e36b4ceef8c38048bc
Baran <brian.baran@torontofilmschool.ca>
2019-10-08 12:43:36 -0400

    bb

---
+++ Capstone/Assets/Oliver's Camera/Script/CameraPivot.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPivot : MonoBehaviour
+{
+    public GameObject target;
+    private PlayerCamera _camera;
+    public float offSetUp = 0.6f;
+
+    public bool runFunc = false;
+    public float DemoVarOffup = 0.6f;
+    public float DemoVarOffleft = 0;
+    public float DemoVarSensi = 3;
+    public float DemoVarDamp = 20;
+    public float DemoVarDis = 6;
+    public float DemoVarMiny = -8;
+    public float DemoVarMaxy = 70;
+    public float DemoVarTrnsSpd = 1;
+
+    void Start()
+    {
+        //Getting Reference to the camera
+        _camera = GetComponentInChildren<PlayerCamera>();
+    }
+
+    void Update()
+    {
+        //Position the camera pivot on the player
+        if (target != null)
+            transform.position = target.transform.position + (Vector3.up * offSetUp);
+
+        //Call Camera Transition
+        if (runFunc)
+        {
+            ChangePlayerCamera(DemoVarOffup, DemoVarOffleft, DemoVarSensi, DemoVarDamp, DemoVarDis, DemoVarMiny, DemoVarMaxy, DemoVarTrnsSpd);
+            runFunc = false;
+        }
+    }
+
+
+    
+
+
+    // Camera Transition //////////////
+
+    public void ChangePlayerCamera(float pOffSetUp, float pOffSetLeft, float pMouseSensitivity, float pTurnDampening, float pCameraDistance, float pCameraMinHeight, float pCameraMaxHeight, float pTransitionSpeed)
+    {
+        StopCoroutine("CameraOffSetTransition");
+        StartCoroutine(CameraOffSetTransition(pOffSetUp, pTransitionSpeed));
+        _camera.ChangePlayerCamera(pOffSetLeft, pMouseSensitivity, pTurnDampening, pCameraDistance, pCameraMinHeight, pCameraMaxHeight, pTransitionSpeed);
+    }
+
+    private IEnumerator CameraOffSetTransition(float pOffSetUp, float pTransitionSpeed)
+    {
+        while (offSetUp != pOffSetUp)
+        {
+            offSetUp = Mathf.Lerp(offSetUp, pOffSetUp, pTransitionSpeed * Time.deltaTime);
+            if (Mathf.Abs(offSetUp - pOffSetUp) <= 0.01f)
+                offSetUp = pOffSetUp;
+
+            yield return null;
+        }
+    }
+}
---
+++ Capstone/Assets/Oliver's Camera/Script/PlayerCamera.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private Transform _CameraTansform;
+    private Transform _ParentTransform;
+    private Vector3 _LocalRotation;
+    private Vector3 _TargetLocalPosition;
+
+    [Header("Camera Collision")]
+    [SerializeField] private LayerMask cameraCollisionLayers;
+    [SerializeField] private float cameraCollisionDampening = 20;
+    [SerializeField] [Range(0, 1)] private float cameraCollisionMinDisPercent = 0.1f;
+
+    [Space]
+    public float MouseSensitivity = 4f;
+    public float TurnDampening = 10f;
+    [SerializeField] private float OffSetLeft = 0f;
+    [SerializeField] private float CameraDistance = 6f;
+    [SerializeField] private float CameraMinHeight = -20f;
+    [SerializeField] private float CameraMaxHeight = 90f;
+
+    public bool CameraDisabled = false;
+    
+    void Start()
+    {
+        //Getting Transforms
+        _CameraTansform = transform;
+        _ParentTransform = transform.parent;
+
+        //Maintaining Starting Rotation
+        _LocalRotation.x = _ParentTransform.eulerAngles.y;
+        _LocalRotation.y = _ParentTransform.eulerAngles.x;
+
+        //Locking cursor
+        Cursor.lockState = CursorLockMode.Locked;
+
+        //Setting camera distance
+        _TargetLocalPosition = new Vector3(-OffSetLeft, 0f, CameraDistance * -1f);
+        _CameraTansform.localPosition = _TargetLocalPosition;
+    }
+
+    void Update()
+    {
+        //Getting Mouse Movement
+        if (!CameraDisabled)
+        {
+            DefaultCameraMovement();
+            CameraCollision();
+        }
+
+        //Actual Camera Transformations
+        Quaternion TargetQ = Quaternion.Euler(_LocalRotation.y, _LocalRotation.x, 0);
+        _ParentTransform.rotation = Quaternion.Lerp(_ParentTransform.rotation, TargetQ, Time.deltaTime * TurnDampening);
+    }
+
+    void DefaultCameraMovement()
+    {
+        //Rotation of the camera based on mouse movement
+        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+        {
+            _LocalRotation.x += Input.GetAxis("Mouse X") * MouseSensitivity;
+            _LocalRotation.y -= Input.GetAxis("Mouse Y") * MouseSensitivity;
+
+            //Clamping the y rotation to horizon and not flipping over at the top
+            if (_LocalRotation.y < CameraMinHeight)
+            {
+                _LocalRotation.y = CameraMinHeight;
+            }
+            else if (_LocalRotation.y > CameraMaxHeight)
+            {
+                _LocalRotation.y = CameraMaxHeight;
+            }
+        }
+    }
+
+    void CameraCollision()
+    {
+        //Camera Collision
+        RaycastHit hit;
+        Physics.Raycast(_ParentTransform.position, (_CameraTansform.position - _ParentTransform.position).normalized, out hit, CameraDistance, cameraCollisionLayers);
+
+        if (hit.point != Vector3.zero)
+        {
+            hit.point -= _ParentTransform.position;
+            //Debug.Log(hit.point.magnitude / _TargetLocalPosition.magnitude * 2 * 100 + "%");
+            //Debug.DrawLine(_ParentTransform.position, hit.point + _ParentTransform.position, Color.red, 0.1f);
+            _CameraTansform.localPosition = Vector3.Lerp(_CameraTansform.localPosition, _TargetLocalPosition * Mathf.Clamp((hit.point.magnitude / _TargetLocalPosition.magnitude), cameraCollisionMinDisPercent, 0.5f), Time.deltaTime * cameraCollisionDampening);
+        }
+        else
+        {
+            _CameraTansform.localPosition = Vector3.Lerp(_CameraTansform.localPosition, _TargetLocalPosition * 0.5f, Time.deltaTime * cameraCollisionDampening);
+        }
+    }
+
+
+
+    // Camera Transition //////////////
+
+    public void ChangePlayerCamera(float pOffSetLeft, float pMouseSensitivity, float pTurnDampening, float pCameraDistance, float pCameraMinHeight, float pCameraMaxHeight, float pTransitionSpeed)
+    {
+        StopCoroutine("OtherCameraVarsTransition");
+        StartCoroutine(OtherCameraVarsTransition(pOffSetLeft, pMouseSensitivity, pTurnDampening, pCameraDistance, pCameraMinHeight, pCameraMaxHeight, pTransitionSpeed));
+    }
+
+    public IEnumerator OtherCameraVarsTransition(float pOffSetLeft, float pMouseSensitivity, float pTurnDampening, float pCameraDistance, float pCameraMinHeight, float pCameraMaxHeight, float pTransitionSpeed)
+    {
+        while (MouseSensitivity != pMouseSensitivity || TurnDampening != pTurnDampening || CameraDistance != pCameraDistance
+                || CameraMinHeight != pCameraMinHeight || CameraMaxHeight != pCameraMaxHeight || OffSetLeft != pOffSetLeft)
+        {
+            //Lerping all of the values
+            MouseSensitivity = Mathf.Lerp(MouseSensitivity, pMouseSensitivity, pTransitionSpeed * Time.deltaTime);
+            if (Mathf.Abs(MouseSensitivity - pMouseSensitivity) <= 0.01f)
+                MouseSensitivity = pMouseSensitivity;
+
+            TurnDampening = Mathf.Lerp(TurnDampening, pTurnDampening, pTransitionSpeed * Time.deltaTime);
+            if (Mathf.Abs(TurnDampening - pTurnDampening) <= 0.01f)
+                TurnDampening = pTurnDampening;
+
+            CameraDistance = Mathf.Lerp(CameraDistance, pCameraDistance, pTransitionSpeed * Time.deltaTime);
+            if (Mathf.Abs(CameraDistance - pCameraDistance) <= 0.01f)
+                CameraDistance = pCameraDistance;
+
+            CameraMinHeight = Mathf.Lerp(CameraMinHeight, pCameraMinHeight, pTransitionSpeed * Time.deltaTime);
+            if (Mathf.Abs(CameraMinHeight - pCameraMinHeight) <= 0.01f)
+                CameraMinHeight = pCameraMinHeight;
+
+            CameraMaxHeight = Mathf.Lerp(CameraMaxHeight, pCameraMaxHeight, pTransitionSpeed * Time.deltaTime);
+            if (Mathf.Abs(CameraMaxHeight - pCameraMaxHeight) <= 0.01f)
+                CameraMaxHeight = pCameraMaxHeight;
+
+            OffSetLeft = Mathf.Lerp(OffSetLeft, pOffSetLeft, pTransitionSpeed * Time.deltaTime);
+            if (Mathf.Abs(OffSetLeft - pOffSetLeft) <= 0.01f)
+                OffSetLeft = pOffSetLeft;
+
+            //Setting camera distance
+            _TargetLocalPosition = new Vector3(-OffSetLeft, 0f, CameraDistance * -1f);
+
+            yield return null;
+        }
+    }
+}
